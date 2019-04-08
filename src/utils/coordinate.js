a9fc81d7d7ca8859ffde2d7160aa9ba283793f3c
无惟 <guangbin.lianggb@alibaba-inc.com>
2019-04-08 14:28:31 +0800

    新增：画布跟随鼠标的位置作为中心点缩放

--- src/utils/coordinate.js
+++ src/utils/coordinate.js
@@ -4,6 +4,8 @@ const _ = require('lodash');
 
 class CoordinateService {
   constructor(opts) {
+    this.wrapper = opts.wrapper;
+    this.girdWrapper = opts.girdWrapper;
     this.terOffsetX = opts.terOffsetX || 0;
     this.terOffsetY = opts.terOffsetY || 0;
     this.terWidth = opts.terWidth || 0;
@@ -11,6 +13,8 @@ class CoordinateService {
     this.canOffsetX = opts.canOffsetX || 0;
     this.canOffsetY = opts.canOffsetY || 0;
     this.scale = opts.scale || 1;
+    this.transformOriginX = this.terWidth / 2;
+    this.transformOriginY = this.terHeight / 2;
   }
 
   _changeCanvasInfo(data) {
@@ -35,6 +39,29 @@ class CoordinateService {
     if (data.scale) {
       this.scale = data.scale;
     }
+    if (data.wrapper) {
+      this.wrapper = data.wrapper;
+    }
+    if (data.girdWrapper) {
+      this.girdWrapper = data.girdWrapper;
+    }
+    let _istransformOriginXChange = false;
+    if (data.mouseX) {
+      if (this.transformOriginX !== data.mouseX - this.terOffsetX) {
+        this.transformOriginX = data.mouseX - this.terOffsetX;
+        _istransformOriginXChange = true;
+      }
+    }
+    if (data.mouseY) {
+      if (this.transformOriginY !== data.mouseY - this.terOffsetY) {
+        this.transformOriginY = data.mouseY - this.terOffsetY;
+        _istransformOriginXChange = true;
+      }
+    }
+    if (_istransformOriginXChange) {
+      this.wrapper.style.transformOrigin = `${this.transformOriginX}px ${this.transformOriginY}px`;
+      this.girdWrapper.style.transformOrigin = `${this.transformOriginX}px ${this.transformOriginY}px`;
+    }
   }
   canvas2terminal(coordinates, options) {
     return [this._canvas2terminal('x', coordinates[0], options), this._canvas2terminal('y', coordinates[1], options)];
@@ -49,12 +76,12 @@ class CoordinateService {
     let terOffsetX = _.get(options, 'terOffsetX') !== undefined ? _.get(options, 'terOffsetX') : this.terOffsetX;
     let terOffsetY = _.get(options, 'terOffsetY') !== undefined ? _.get(options, 'terOffsetY') : this.terOffsetY;
     if (pos === 'x') {
-      const terCenter = terOffsetX + this.terWidth / 2;
-      return (coordinate - this.terWidth / 2) * scale + terCenter + canOffsetX;
+      const terCenter = terOffsetX + this.transformOriginX;
+      return (coordinate - this.transformOriginX) * scale + terCenter + canOffsetX;
     }
     if (pos === 'y') {
-      const terCenter = terOffsetY + this.terHeight / 2;
-      return (coordinate - this.terHeight / 2) * scale + terCenter + canOffsetY;
+      const terCenter = terOffsetY + this.transformOriginY;
+      return (coordinate - this.transformOriginY) * scale + terCenter + canOffsetY;
     }
   }
 
@@ -65,14 +92,14 @@ class CoordinateService {
     let terOffsetX = _.get(options, 'terOffsetX') !== undefined ? _.get(options, 'terOffsetX') : this.terOffsetX;
     let terOffsetY = _.get(options, 'terOffsetY') !== undefined ? _.get(options, 'terOffsetY') : this.terOffsetY;
     if (pos === 'x') {
-      const terCenter = terOffsetX + this.terWidth / 2;
+      const terCenter = terOffsetX + this.transformOriginX;
       // console.log((coordinate - terCenter - this.canOffsetX) / this.scale + this.terWidth / 2);
-      return (coordinate - terCenter - canOffsetX) / scale + this.terWidth / 2;
+      return (coordinate - terCenter - canOffsetX) / scale + this.transformOriginX;
     }
     if (pos === 'y') {
-      const terCenter = terOffsetY + this.terHeight / 2;
+      const terCenter = terOffsetY + this.transformOriginY;
       // console.log((coordinate - terCenter - this.canOffsetY) / this.scale + this.terHeight / 2);
-      return (coordinate - terCenter - canOffsetY) / scale + this.terHeight / 2;
+      return (coordinate - terCenter - canOffsetY) / scale + this.transformOriginY;
     }
   }
 }
