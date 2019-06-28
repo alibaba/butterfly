9566ee1548f9c24ddee3b9d2009ce6c6d12bd30f
无惟 <guangbin.lianggb@alibaba-inc.com>
2019-06-28 10:29:43 +0800

    整理：文档接口

---
+++ src/interface/edge.js
@@ -0,0 +1,81 @@
+'use strict';
+
+class Edge {
+  constructor() {
+    // id                 节点唯一标志
+    // targetNode         目标节点
+    // targetEndpoint     目标锚点
+    // sourceNode         源节点
+    // sourceEndpoint     源锚点
+    // type               "node" or "endpoint"，标志类型的
+    // shapeType          线条类型
+    // label              设置线条上的字体
+    // options            数据的透传
+    // dom                线条dom
+    // labelDom           label的dom
+    // arrowDom           箭头的dom
+
+    
+    // 箭头部分看看需要优化不？
+    // arrow              是否有箭头
+    // arrowPosition      箭头的位置
+    // arrowOffset        箭头起始点
+
+    // 需要优化的
+    // isExpandWidth      拓展线条
+    // eventHandlerDom    代替绑定事件的dom
+    // orientationLimit   限制方向
+
+
+    // 需要优化的
+    // scope           scope相同可拉进group里面
+    // endpoints       endpoint对象
+    // _endpointsData  真实的endpoint数据
+
+    // 需要悬空
+  }
+
+  // 渲染节点
+  draw() {}
+
+  // 重绘节点
+  redraw() {}
+
+  // 渲染label
+  drawLabel() {}
+
+  // 重绘label
+  redrawLabel() {}
+
+  // 渲染arrow
+  drawArrow() {}
+
+  // 重回arrow
+  redrawArrow() {}
+
+  // 判断是否能连接的方法
+  isConnect() {}
+
+  // 销毁节点
+  destroy() {}
+
+  // ********* 需要新增的api ********* 
+
+  // 删除线条
+  remove() {}
+
+  // 单击的回调
+  click() {}
+
+  // hover的回调
+  hover() {}
+
+  // focus回调
+  focus() {}
+
+  // unFocus回调
+  unFocus() {}
+
+}
+
+module.exports = Edge;
--- src/interface/group.js
+++ src/interface/group.js
@@ -1,46 +1,98 @@
 'use strict';
 
 class Group {
-  constructor() {}
+  constructor() {
+    // id        节点唯一标志
+    // top       坐标y
+    // left      坐标x
+    // width     节点组宽度
+    // height    节点组高度
+    // resize    标志是否能resize
+    // dom       实际的dom
+    // nodes     里面包含的节点对象
+    // options   数据的透传
+    // _on       节点发送事件
+    // _emit     节点发送事件
+    // _global   全局的配置
 
-  /**
-   * 渲染节点
-   * @param {object} obj - 节点的数据结构
-   * @return {dom} dom - 返回的dom会渲染到图上
-   */
-  draw(obj) {}
 
-  /**
-   * 添加节点
-   * @param {array} node - 添加多个节点到节点组里面
-   */
-  addNode(node) {}
 
-  /**
-   * 添加节点
-   * @param {array} nodes - 添加多个节点到节点组里面
-   */
-  addNodes(nodes) {}
+    // 需要优化的
+    // scope           scope相同可拉进group里面
+    // endpoints       endpoint对象
+    // _endpointsData  真实的endpoint数据
 
-  /**
-   * 设置是否可缩放
-   * @param {boolean} flat - 是否缩放的标记
-   */
-  setResize(flat) {}
+  }
 
-  /**
-   * remove的方法
-   */
+  // 渲染节点
+  draw() {}
+
+  // 添加节点
+  addNode() {}
+
+  // 批量添加节点
+  addNodes() {}
+
+  // 获取宽度
+  getWidth() {}
+
+  // 获取高度
+  getHeight() {}
+
+  // 删除节点
+  removeNode() {}
+
+  // 批量删除节点
+  removeNodes() {}
+
+  // 设置是否可缩放
+  setResize() {}
+
+  // 设置大小
+  setSize() {}
+  
+  // 移动group
+  moveTo() {}
+
+  // 获取锚点
+  getEndpoint() {}
+
+  // 添加锚点
+  addEndpoint() {}
+
+  // remove的方法
   remove() {}
 
-  /**
-   * focus回调
-   */
+  // 销毁的方法
+  destroy() {}
+
+  // ********* 需要新增的api ********* 
+
+  // 删除锚点
+  removeEndpoint() {}
+
+  // 折叠的方法
+  collapse() {}
+
+  // 伸展的方法
+  stretch() {}
+
+  // 单击的回调
+  click() {}
+
+  // 双击的回调
+  doubleClick() {}
+
+  // 右键的回调
+  onContextmenu() {}
+
+  // hover的回调
+  hover() {}
+
+  // focus回调
   focus() {}
 
-  /**
-   * unFocus回调
-   */
+  // unFocus回调
   unFocus() {}
 }
 
