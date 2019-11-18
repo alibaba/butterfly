db757b02fff77ae5989144244ef80e57868eb13b
Gorgear <384113872@qq.com>
2019-11-18 20:56:11 +0800

    feat: 更完美的minimap

--- src/canvas/baseCanvas.js
+++ src/canvas/baseCanvas.js
@@ -1209,6 +1209,10 @@ class BaseCanvas extends Canvas {
   }
 
   setMinimap(flat = true, options = {}) {
+    if(!options.events) {
+      options.events = {};
+    }
+
     const updateEvts = [
       'system.canvas.zoom',
       'system.node.delete',
@@ -1217,7 +1221,8 @@ class BaseCanvas extends Canvas {
       'system.group.delete',
       'system.group.move',
       'system.drag.move',
-      'system.canvas.move'
+      'system.canvas.move',
+      ...options.events
     ];
 
     const getNodes = () => {
@@ -1228,9 +1233,23 @@ class BaseCanvas extends Canvas {
           top: node.top,
           width: node.getWidth(),
           height: node.getHeight(),
-          group: node.group
+          group: node.group,
+          minimapActive: _.get(node, 'options.minimapActive')
         }
       });
+    }
+
+    const getGroups = () => {
+      return this.groups.map(group => {        
+        return {
+          id: group.id,
+          left: group.left,
+          top: group.top,
+          width: group.getWidth(),
+          height: group.getHeight(),
+          minimapActive: _.get(group, 'options.minimapActive')
+        }
+      });      
     }
 
     if(flat && !this.minimap) {
@@ -1238,8 +1257,9 @@ class BaseCanvas extends Canvas {
         root: this.root,
         move: this.move.bind(this),
         terminal2canvas: this.terminal2canvas.bind(this),
+        canvas2terminal: this.canvas2terminal.bind(this),
         nodes: getNodes(),
-        groups: this.groups,
+        groups: getGroups(),
         zoom: this.getZoom(),
         offset: this.getOffset(),
         ...options
@@ -1248,7 +1268,7 @@ class BaseCanvas extends Canvas {
       this.updateFn = () => {
         this.minimap.update({
           nodes: getNodes(),
-          groups: this.groups,
+          groups: getGroups(),
           zoom: this.getZoom(),
           offset: this.getOffset()
         });
--- src/utils/minimap.js
+++ src/utils/minimap.js
@@ -1,10 +1,12 @@
 const _ = require('lodash');
 const $ = require('jquery');
 
-
 // 每一个dot是一个圆形或者
 const DOT_COLOR = 'rgba(76, 158, 164, 1)';
 const GROUP_COLOR = 'rgba(61, 86, 92, 1)';
+const DOT_ACTIVE_COLOR = 'rgba(255, 253, 76, 1)';
+const GROUP_ACTIVE_COLOR = 'rgba(255, 253, 76, 1)';
+const SAFE_DISTANCE = 20;
 
 // 修改一个element的css的属性
 const modifyCSS = (ele, cssStyle) => {
@@ -66,15 +68,21 @@ const checkOpts = (options) => {
  *  groups {Object[]} 节点组信息
  *    group.left, group.top, group.width, group.height 节点组的二维信息
  *  offset {Pointer} 偏移信息
- *  zoom {Nuber} 画布当前缩放比
+ *  zoom {Number} 画布当前缩放比
  *  move {Function} 缩略图互动函数, 用于移动画布, 参考小蝴蝶的move
  *  terminal2canvas {Function} 互动函数, 屏幕坐标到画布坐标的转换
+ *  safeDistance {Number} 画布视口在minimap距离边距的安全距离，默认20
+ *  activeNodeColor {String} 选中的节点颜色
+ *  activeGroupColor {String} 选中的节点组颜色
+ *  events {String[]} 补充的监听事件
  */
 class Minimap {
   constructor(options) {
     checkOpts(options);
 
     this.root = options.root;
+    this.$root = $(this.root);
+
     this.options = {
       height: 200,
       width: 200,
@@ -84,6 +92,8 @@ class Minimap {
       backgroudStyle: {},
       nodeColor: DOT_COLOR,
       groupColor: GROUP_COLOR,
+      activeNodeColor: DOT_ACTIVE_COLOR,
+      activeGroupColor: GROUP_ACTIVE_COLOR,
       containerWidth: $(this.root).width(),
       containerHeight: $(this.root).height(),
       nodes: [],
@@ -92,6 +102,7 @@ class Minimap {
       zoom: 1,
       move: () => null,
       terminal2canvas: () => null,
+      safeDistance: SAFE_DISTANCE,
       ...options
     };
 
@@ -106,82 +117,122 @@ class Minimap {
     this.renderBG();
   }
 
+
+  // 获取所有元素的屏幕坐标
+  getItemsPoint = () => {
+    const nodes = _.cloneDeep(this.options.nodes);
+    const groups = _.cloneDeep(this.options.groups);
+    const {canvas2terminal} = this.options;
+    const height = this.$root.height();
+    const width = this.$root.width();
+
+    nodes.push({left: 0, top: 0, height: 1, width: 1});
+    nodes.push({left: width, top: height, height: 1, width: 1});
+
+    // 计算所有 nodes 的真实坐标
+    for(let node of nodes) {
+      if(node.group) {
+        const group = _.find(groups, {id: node.group});
+
+        if(!group) {
+          continue;
+        }
+
+        node.rleft = group.left + node.left;
+        node.rtop = group.top + node.top;
+
+        continue;
+      }
+
+      node.rleft = node.left;
+      node.rtop = node.top;
+    }
+
+    groups.forEach(group => {
+      const leftTop = [group.left, group.top];
+      const rightBottom = [group.left + group.width, group.top + group.height];
+
+      const screenLeftTop = canvas2terminal(leftTop);
+      const screenRightBottom =  canvas2terminal(rightBottom);
+
+      group.screenLeftTop = screenLeftTop;
+      group.screenRightBottom = screenRightBottom;
+
+      group.left = screenLeftTop[0];
+      group.top = screenLeftTop[1];
+      group.width = screenRightBottom[0] - screenLeftTop[0];
+      group.height = screenRightBottom[1] - screenLeftTop[1];
+    });
+
+    nodes.forEach(node => {
+      const leftTop = [node.rleft, node.rtop];
+      const rightBottom = [node.rleft + node.width, node.rtop + node.height];
+
+      const screenLeftTop = canvas2terminal(leftTop);
+      const screenRightBottom =  canvas2terminal(rightBottom);
+
+      node.left = screenLeftTop[0];
+      node.top = screenLeftTop[1];
+      node.width = screenRightBottom[0] - screenLeftTop[0];
+      node.height = screenRightBottom[1] - screenLeftTop[1];
+    });
+
+    return {
+      groups, nodes
+    }
+  }
+
   // 获取画布的有内容的区域
   getBBox() {
-    const {nodes, groups} = this.options;
+    const {nodes, groups} = this.getItemsPoint()
+
     const check = (v) => {
       return _.isNumber(v) ? v : 0;
     }
 
     const allNTop = nodes.map(node => node.top);
     const allNLeft = nodes.map(node => node.left);
+    const allNBottom = nodes.map(node => node.top + node.height);
+    const allNRight = nodes.map(node => node.left + node.width);
 
+    const allGTop = groups.map(group => group.top);
+    const allGLeft = groups.map(group => group.left);
     const allGBottom = groups.map(group => group.top + group.height);
-    const allGRight = groups.map(group => group.right + group.width);
+    const allGRight = groups.map(group => group.left + group.width);
 
-    const allTop = allNTop.concat(allGBottom);
-    const allLeft = allNLeft.concat(allGRight);
+    const allTop = allNTop.concat(allGTop);
+    const allLeft = allNLeft.concat(allGLeft);
+
+    const allBottom = allNBottom.concat(allGBottom);
+    const allRight = allNRight.concat(allGRight);
 
     const minX = check(_.min(allLeft));
-    const maxX = check(_.max(allLeft));
     const minY = check(_.min(allTop));
-    const maxY = check(_.max(allTop));
+
+    const maxX = check(_.max(allRight));
+    const maxY = check(_.max(allBottom));
 
     return {
       minX: minX,
-      minY: minX,
+      minY: minY,
       width: maxX - minX,
       height: maxY - minY,
     }
-  }
-
-  // 画布size
-  getCanvasSize = () => {
-    return {
-      width: this.options.containerWidth,
-      height: this.options.containerHeight
-    }
-  }
-
-  // 可视区域size
-  getGraphSize() {
-    // 不写0防止除0错误
-    let width = 1;
-    let height = 1;
-    let minX = 0;
-    let minY = 0;
-
-    const bbox = this.getBBox();
-    const gSize = this.getCanvasSize();
-
-    try {
-      width = _.max([
-        gSize.width, bbox.width
-      ]);
-
-      height = _.max([
-        gSize.height, bbox.height
-      ]);
-
-      minX = _.min([bbox.minX, 0]);
-      minY = _.min([bbox.minY, 0]);
-    } catch (e) {
-      console.log(e); // eslint-disable-line
-    }
-
-    return {
-      width, height, minX, minY
-    };
   }
 
   // 获取画布和缩略图的缩放比
   setRatio() {
     const height = this.options.height;
     const width = this.options.width;
-    const graphSize = this.getGraphSize();
+    const graphSize = this.getBBox();
+
+    if(graphSize.width === 0 || graphSize.height === 0) {
+      return 0;
+    }
+
     const ratio = Math.min(width / graphSize.width, height / graphSize.height);
 
-    this.ratio = ratio;    
+    this.ratio = Number(ratio.toFixed(2));
   }
 
   // 更新小地图数据
@@ -197,6 +248,24 @@ class Minimap {
     this.options.offset = offset;
 
     this.debounceRender();
+  }
+
+  getViewportBBox = () => {
+    const $viewport = $(this.viewportDOM);
+    const parent = $viewport.offsetParent();
+    const offset = $viewport.offset();
+    const poffset = $(parent).offset();
+    const left = offset.left - poffset.left;
+    const top = offset.top - poffset.top;
+
+    const width = $viewport.width();
+    const height = $viewport.height();
+    const right = left + width;
+    const bottom = top + height;
+
+    return {
+      left, top, right,bottom, height, width
+    }
   }
 
   // 初始化画布
@@ -227,8 +296,8 @@ class Minimap {
       bottom: '10px',
       left: 'none',
       top: 'none',
-      height: height + 2 + 'px',
-      width: width + 2 + 'px',
+      height:  height + 'px',
+      width: width + 'px',
       border: '1px solid #aaa',
       'z-index': 100,
       ...containerStyle
@@ -240,12 +309,14 @@ class Minimap {
       top: 0,
       // border: '1px solid pink',
       'background-color': 'rgba(79, 111, 126, 0.4)',
-      viewportStyle,
+      ...viewportStyle,
     });
 
-    modifyCSS(this.backgroundDOM , {
+    modifyCSS(this.backgroundDOM , {      
       ...initStyle,
-      ...backgroudStyle
+      ...backgroudStyle,
+      height: height + 'px',
+      width: width + 'px',      
     });
 
     this.root.appendChild(this.container);
@@ -289,21 +360,26 @@ class Minimap {
 
     this.viewportEvents = {
       mousedown: (e) => {
+        e.preventDefault();
+        e.stopPropagation();
         const viewportDOM = this.viewportDOM;
 
         if (e.target !== this.viewportDOM) {
           return;
         }
 
-        const style = viewportDOM.style;
+        const viewportBBox = this.getViewportBBox();
 
-        left = parseInt(style.left, 10);
-        top = parseInt(style.top, 10);
+        left = parseInt(viewportBBox.left, 10);
+        top = parseInt(viewportBBox.top, 10);
+
         dragging = true;
         x = e.clientX;
         y = e.clientY;
       },
       mousemove: (e) => {
+        e.preventDefault();
+        e.stopPropagation();
         if (!dragging || _.isNil(e.clientX) || _.isNil(e.clientY)) {
           return;
         }
@@ -311,28 +387,42 @@ class Minimap {
         let dx = x - e.clientX;
         let dy = y - e.clientY;
 
-        if (left - dx < 0) {
-          dx = left;
-        }
-
-        if (top - dy < 0) {
-          dy = top;
-        }
-
         left -= dx;
         top -= dy;
+
+        const {width: mapWidth, height: mapHeight, safeDistance} = this.options;
+        const {width: vpWidth, height: vpHeight} = this.getViewportBBox();
+
+        // 限制视口不能够超出 minimap 的安全距离
+        if(
+          left >= mapWidth - safeDistance ||
+          top >= mapHeight - safeDistance ||
+          left + vpWidth <= safeDistance ||
+          top + vpHeight <= safeDistance
+        ) {
+          left += dx;
+          top += dy;
+
+          return;
+        }
+
 
         modifyCSS(this.viewportDOM, {
           left: left + 'px',
           top: top + 'px'
         });
 
-        const zoom = this.options.zoom;
+        const {minX, minY} = this.getBBox();
+        const offset = this.$root.offset();
+        const boffset = this.options.offset;
 
+        const ddx = (((-left / this.ratio) + offset.left) - minX) * this.ratio * -1
+        const ddy = (((-top / this.ratio) + offset.top) - minY) * this.ratio * -1
+        
         this.options.move(
           [
-            (-left / this.ratio) * zoom,
-            (-top / this.ratio) * zoom
+            boffset[0] + -ddx / this.ratio,
+            boffset[1] + -ddy / this.ratio
           ]
         );
 
@@ -355,80 +445,70 @@ class Minimap {
   // 渲染缩略图canvas
   renderBG() {
     const cvsCtx = this.cvsCtx;
-    const {width, height, nodeColor, groupColor} = this.options;
+    const {width, height, nodeColor, groupColor, activeNodeColor, activeGroupColor} = this.options;
     const cvsRatio = this.cvsRatio;
 
     cvsCtx.clearRect(0, 0, width, height);
 
     // 根据所有点的信息画出所有的点
-    const nodes = this.options.nodes;
-    const groups = this.options.groups;
+    const {nodes, groups} = this.getItemsPoint();
+    const {minX, minY} = this.getBBox();
 
-    // 计算所有 nodes 的真实坐标
-    for(let node of nodes) {
-      if(node.group) {
-        const group = _.find(groups, {id: node.group});
-
-        if(!group) {
-          continue;
-        }
-
-        node.rleft = group.left + node.left;
-        node.rtop = group.top + node.top;
-
-        continue;
-      }
-
-      node.rleft = node.left;
-      node.rtop = node.top;
-    }
-
-
-    cvsCtx.fillStyle = groupColor;
     groups.forEach(group => {
-      const left = group.left * this.ratio ;
-      const top = group.top * this.ratio;
+      const left = (group.left - minX) * this.ratio;
+      const top = (group.top - minY) * this.ratio;
       const width = group.width * this.ratio;
       const height = group.height * this.ratio;
+      const minimapActive = group.minimapActive;
+
+      if(minimapActive) {
+        cvsCtx.fillStyle = activeGroupColor;
+      } else {
+        cvsCtx.fillStyle = groupColor;
+      }
 
       cvsCtx.fillRect(left / cvsRatio, top / cvsRatio, width / cvsRatio, height / cvsRatio);
     });
 
-    cvsCtx.fillStyle = nodeColor;
-
     nodes.forEach(node => {
-      const left = node.rleft * this.ratio;
-      const top = node.rtop * this.ratio;
+      const left = (node.left - minX) * this.ratio;
+      const top = (node.top - minY) * this.ratio;
       const width = node.width * this.ratio;
       const height = node.height * this.ratio;
+
+      if(node.minimapActive) {
+        cvsCtx.fillStyle = activeNodeColor;
+      } else {
+        cvsCtx.fillStyle = nodeColor;
+      }
 
       // cvsCtx.beginPath();
       cvsCtx.fillRect(left / cvsRatio, top / cvsRatio, width / cvsRatio, height / cvsRatio);
       // cvsCtx.arc(left / cvsRatio, top / cvsRatio, DOT_SIZE / cvsRatio, 0, 2*Math.PI);
       // cvsCtx.closePath();
       // cvsCtx.fill();
-    });    
-
+    });
   }
 
   // 绘制出拖动框
   renderViewPort() {
     this.setRatio();
-    const {width, height} = this.options;
-    const graphSize = this.getCanvasSize();
-    const topLeft = this.options.terminal2canvas([0, 0]);
-    const bottomRight = this.options.terminal2canvas([graphSize.width, graphSize.height]);
 
-    const offset = this.options.offset;
-    const zoom = this.options.zoom;
+    const {minX, minY} = this.getBBox();
+
+    const {top, left} = this.$root.offset();
+
+    const rootWidth = this.$root.width();
+    const rootHeight = this.$root.height();
+    const offset = [left - minX, top - minY];
 
     // 获取画布到minimap的缩放比
     const ratio = this.ratio;
 
-    const vwidth = _.min([(bottomRight[0] - topLeft[0]) * ratio, width]);
-    const vheight = _.min([(bottomRight[1] - topLeft[1]) * ratio, height]);
-    const vleft = -((offset[0] / zoom) * ratio);
-    const vtop = -((offset[1] / zoom) * ratio);
+    const vwidth = rootWidth * this.ratio;
+    const vheight = rootHeight * this.ratio;
+    const vleft = Math.round(Math.round(offset[0]) * ratio);
+    const vtop = Math.round(Math.round(offset[1]) * ratio);
 
     modifyCSS(this.viewportDOM, {
       width: `${vwidth}px`,
