b063df20868173bc2aca4cd3a368863a53119742
隋航 <wb-sh596298@alibaba-inc.com>
2020-06-18 14:35:58 +0800

    feat: 将添加节点后drage的重新布局转到业务中实现

--- example/demo/drageLayout/index.jsx
+++ example/demo/drageLayout/index.jsx
@@ -1,6 +1,7 @@
 'use strict';
 import React, {Component} from 'react';
-const DrageCanvas = require('../../../index.js').DrageCanvas;
+const Canvas = require('../../../index.js').Canvas;
+const Layout = require('../../../index.js').Layout;
 const mockData = require('./data.js');
 const RelationEdge = require('./edge_relation.jsx');
 const Train = require('./node_train.jsx');
@@ -15,7 +16,7 @@ class DrageLayout extends Component {
   }
   componentDidMount() {
     let root = document.getElementById('dag-canvas');
-    this.canvas = new DrageCanvas({
+    this.canvas = new Canvas({
       root: root,
       disLinkable: true, // 可删除连线
       linkable: false,    // 可连线
@@ -70,8 +71,43 @@ class DrageLayout extends Component {
         source: 'test2',
         target: 'test7',
       },
-    ])
-    this.canvas.drageReDraw({rankdir: ''});
+    ]);
+    // 重新布局需要自己在业务中实现
+    this.drageReDraw();
+  }
+  drageReDraw(newParam) {
+    let {nodes, layout, edges} = this.canvas;
+    let addResultNodes = nodes.map((item) => {
+      return item.options
+    });
+    Layout.drageLayout({
+      rankdir: (newParam && newParam.rankdir) || _.get(layout, 'options.rankdir') || 'TB',
+      align: (newParam && newParam.align) || _.get(layout, 'options.align'),
+      nodeSize: (newParam && newParam.nodeSize) || _.get(layout, 'options.nodeSize'),
+      nodesepFunc: (newParam && newParam.nodesepFunc) || _.get(layout, 'options.nodesepFunc'),
+      ranksepFunc: (newParam && newParam.ranksepFunc) || _.get(layout, 'options.ranksepFunc'),
+      nodesep: (newParam && newParam.nodesep) || _.get(layout, 'options.nodesep') || 50,
+      ranksep: (newParam && newParam.ranksep) || _.get(layout, 'options.ranksep') || 50,
+      controlPoints: (newParam && newParam.controlPoints) || _.get(layout, 'options.controlPoints') || false,
+      data: {
+        nodes: addResultNodes,
+        edges: edges.map(item => ({
+          source: item.type === 'endpoint' ? item.sourceNode : item.sourceNode.id,
+          target: item.type === 'endpoint' ? item.targetNode : item.targetNode.id
+        }))
+      }});
+    // 布局计算完位置后left和top赋值给node节点
+    nodes.forEach((item, index) => {
+      let currentNodeNewLeft = addResultNodes[index].left;
+      let currentNodeNewTop = addResultNodes[index].top;
+      let currentNodeNewPosInfo = addResultNodes[index].posInfo;
+      if (item.top !== currentNodeNewTop || item.left !== currentNodeNewLeft) {
+        item.options.top = currentNodeNewTop;
+        item.options.left = currentNodeNewLeft;
+        item.options.posInfo = currentNodeNewPosInfo;
+        item.moveTo(currentNodeNewLeft, currentNodeNewTop);
+      }
+    });
   }
   render() {
     return (
