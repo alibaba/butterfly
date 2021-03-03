24ef27df533913f0273c8b153f80fcd1b6ccaa70
Gorgear <384113872@qq.com>
2021-03-03 14:38:44 +0800

    feat: 新首页

--- example/pages/home/index.jsx
+++ example/pages/home/index.jsx
@@ -11,17 +11,17 @@ const features = [
   {
     name: i18next.t('home_out_of_the_box'),
     description: i18next.t('home_box_desc'),
-    img: 'https://gw.alipayobjects.com/zos/bmw-prod/881dc458-f20b-407b-947a-95104b5ec82b/k79dm8ih_w144_h144.png'
+    img: 'https://img.alicdn.com/imgextra/i2/O1CN01v68PpN26oJXK9aZBp_!!6000000007708-2-tps-536-240.png'
   },
   {
     name: i18next.t('home_manage_canvas'),
     description: i18next.t('home_canvas_desc'),
-    img: 'https://gw.alipayobjects.com/zos/bmw-prod/b3e102cd-5dad-4046-a02a-be33241d1cc7/kj9t8oji_w144_h144.png',
+    img: 'https://img.alicdn.com/imgextra/i3/O1CN01jqMnpa21SGOARGFqU_!!6000000006983-2-tps-464-240.png',
   },
   {
     name: i18next.t('home_flexible'),
     description: i18next.t('home_flexible_desc'),
-    img: 'https://gw.alipayobjects.com/zos/bmw-prod/d1ee0c6f-5aed-4a45-a507-339a4bfe076c/k7bjsocq_w144_h144.png'
+    img: 'https://img.alicdn.com/imgextra/i3/O1CN01I6oB8t1PQvq7R6yVK_!!6000000001836-2-tps-570-246.png'
   }
 ];
 
@@ -33,7 +33,8 @@ const Home = () => {
           Butterfly
         </h1>
         <div className="sub-title">
-          🦋{i18next.t('home_sub_title')}
+          {/* 🦋{i18next.t('home_sub_title')} */}
+          {i18next.t('home_sub_title')}
         </div>
         <div>
           <a
@@ -57,18 +58,22 @@ const Home = () => {
               <div
                 className="feature-box"
                 key={feature.name}
-                style={{
-                  backgroundImage: `url(${feature.img})`
-                }}
               >
-                <h3>{feature.name}</h3>
-                <span>{feature.description}</span>
+                <div
+                  className="feature-img"
+                  style={{
+                    backgroundImage: `url(${feature.img})`
+                  }}>
+                </div>
+                <div className="feature-name">{feature.name}</div>
+                <span className="feature-description">{feature.description}</span>
               </div>
             );
           })
         }
       </div>
       <div className="demo-img">
+        <div className="demo-title">{i18next.t('home_example')}</div>
         <img src="https://camo.githubusercontent.com/27d85184fdd0e518610f681ca4249178db75962af2ceb3c3bd6f26469702395a/68747470733a2f2f696d672e616c6963646e2e636f6d2f696d6765787472612f69342f4f31434e303164375748567331766b45447a5752526c575f2121363030303030303030363231302d322d7470732d323430302d383137322e706e67" />
       </div>
     </div>
