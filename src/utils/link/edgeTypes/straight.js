851f96b1a88dbd1397c52221a0ecb6759b0f55a6
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-23 22:57:32 +0800

    Delete src directory

--- src/utils/link/edgeTypes/straight.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function drawStraight(sourcePoint, targetPoint) {
-  let result = ['M', sourcePoint.pos[0], sourcePoint.pos[1], 'L', targetPoint.pos[0], targetPoint.pos[1]];
-  return result.join(' ');
-}
-export default drawStraight;
