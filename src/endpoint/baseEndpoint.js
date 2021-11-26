b26abd76be12d8848728847bbc32abfd89682de4
无惟 <guangbin.lianggb@alibaba-inc.com>
2021-11-26 10:32:57 +0800

    chore: add coordinate func

--- src/endpoint/baseEndpoint.js
+++ src/endpoint/baseEndpoint.js
@@ -102,8 +102,10 @@ class BaseEndpoint extends Endpoint {
       this._height = $(this.dom).outerHeight();
 
       // 计算锚点起始值
-      this._left = this._coordinateService._terminal2canvas('x', $(this.dom).offset().left + this._coordinateService.scrollLeft);
-      this._top = this._coordinateService._terminal2canvas('y', $(this.dom).offset().top + this._coordinateService.scrollTop);
+      // this._left = this._coordinateService._terminal2canvas('x', $(this.dom).offset().left + this._coordinateService.scrollLeft);
+      // this._top = this._coordinateService._terminal2canvas('y', $(this.dom).offset().top + this._coordinateService.scrollTop);
+      this._left = this._coordinateService._terminal2canvas('x', $(this.dom).offset().left);
+      this._top = this._coordinateService._terminal2canvas('y', $(this.dom).offset().top);
 
       this._posLeft = this._left;
       this._posTop = this._top;
