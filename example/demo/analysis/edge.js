c89910469664403d5db484bc8a20347be2a03274
zhang123-bit <1768631571@qq.com>
2022-04-09 11:13:56 +0800

    曼哈顿曲线支持圆角

--- example/demo/analysis/edge.js
+++ example/demo/analysis/edge.js
@@ -5,27 +5,27 @@ import { Edge } from 'butterfly-dag';
 import $ from 'jquery';
 
 class BaseEdge extends Edge {
-  draw(obj) {
-    let path = super.draw(obj);
-    if (this.options.color) {
-      $(path).addClass(this.options.color);
+    draw(obj) {
+        let path = super.draw(obj);
+        if (this.options.color) {
+            $(path).addClass(this.options.color);
+        }
+        return path;
     }
-    return path;
-  }
-  drawArrow(isShow) {
-    let dom = super.drawArrow(isShow);
-    if (this.options.color) {
-      $(dom).addClass(this.options.color);
+    drawArrow(isShow) {
+        let dom = super.drawArrow(isShow);
+        if (this.options.color) {
+            $(dom).addClass(this.options.color);
+        }
+        return dom;
     }
-    return dom;
-  }
-  drawLabel(text) {
-    let dom = null;
-    if (!!text) {
-      dom = $(`<i class="newIconfont iconjiandao-tianchong label ${text}"></i>`)[0];
+    drawLabel(text) {
+        let dom = null;
+        if (!!text) {
+            dom = $(`<i class="newIconfont iconjiandao-tianchong label ${text}"></i>`)[0];
+        }
+        return dom;
     }
-    return dom;
-  }
 };
 
 export default BaseEdge;
