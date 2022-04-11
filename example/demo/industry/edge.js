076a5f3c856608f93f42c0ff80b22b5134575b13
zhang123-bit <1768631571@qq.com>
2022-04-11 10:34:58 +0800

    test

--- example/demo/industry/edge.js
+++ example/demo/industry/edge.js
@@ -1,32 +1,31 @@
 'use strict';
 
-//import { Edge } from 'butterfly-dag';
-import Edge from '../../../src/edge/baseEdge';
+import { Edge } from 'butterfly-dag';
 import $ from 'jquery';
 class BaseEdge extends Edge {
-    draw(obj) {
-        let path = super.draw(obj);
-        if (this.options.color) {
-            $(path).addClass(this.options.color);
-        }
-        return path;
+  draw(obj) {
+    let path = super.draw(obj);
+    if (this.options.color) {
+      $(path).addClass(this.options.color);
     }
-    drawArrow(isShow) {
-        let dom = super.drawArrow(isShow);
-        if (this.options.color) {
-            $(dom).addClass(this.options.color);
-        }
-        return dom;
+    return path;
+  }
+  drawArrow(isShow) {
+    let dom = super.drawArrow(isShow);
+    if (this.options.color) {
+      $(dom).addClass(this.options.color);
     }
-    drawLabel(text) {
-        // let dom = super.drawArrow(text);
-        // console.log(dom);
-        let dom = null;
-        if (!!text) {
-            dom = $(`<span class="label">${text}</span>`)[0];
-        }
-        return dom;
+    return dom;
+  }
+  drawLabel(text) {
+    // let dom = super.drawArrow(text);
+    // console.log(dom);
+    let dom = null;
+    if (!!text) {
+      dom = $(`<span class="label">${text}</span>`)[0];
     }
+    return dom;
+  }
 };
 
 export default BaseEdge;
